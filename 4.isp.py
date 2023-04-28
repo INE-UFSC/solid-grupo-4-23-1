f2f5736437ca2a4a154afa0ba16242bde64ec489
Leonardo Pfeng <col.leonardopfeng@uniuv.edu.br>
2023-04-28 11:29:37 -0300

    4 arrumado

--- 4.isp.py
+++ 4.isp.py
@@ -2,28 +2,16 @@
 Interface Segregation Principle
 Crie interfaces que são específicas. Clientes não devem depender de interfaces que eles não usarão
 """
-from abc import ABC
+from abc import ABC, abstractmethod
 
 class IJanela(ABC):
-    def maximizar(self):
-        raise NotImplementedError
-    
-    def minimizar(self):
-        raise NotImplementedError
-    
-    def mostrar_menu(self):
-        raise NotImplementedError
-        
+
+    @abstractmethod
     def fechar(self):
         raise NotImplementedError
 
 class JanelaTamanhoFixo(IJanela):
-    def maximizar(self):
-        pass
 
-    def minimizar(self):
-        pass
-    
     def mostrar_menu(self):
         pass
     
@@ -31,15 +19,13 @@ class JanelaTamanhoFixo(IJanela):
         pass
 
 class JanelaSemMenu(IJanela):
+    
     def maximizar(self):
         pass
 
     def minimizar(self):
         pass
-    
-    def mostrar_menu(self):
-        pass
-    
+
     def fechar(self):
         pass
 
